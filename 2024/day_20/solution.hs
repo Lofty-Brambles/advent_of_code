ed2812eff2feba270250ab91420bd497b9c007fe
Lofty <prantik.ts@gmail.com>
2024-12-20 18:18:18 +0000

    feat: incomplete day 20

--- 2024/day_20/solution.hs
+++ 2024/day_20/solution.hs
@@ -53,8 +53,8 @@ add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (a, b, c) = f a b c
 
-solve1 :: Input -> Solution
-solve1 = uncurry3 $ cheat 100 2
+solve1 :: Input -> Input
+solve1 x = x -- = uncurry3 $ cheat 100 2
 
 cheat :: Int -> Int -> Maze -> MazeCosts -> MazeCosts -> Int
 cheat threshold l maze costS costE =
