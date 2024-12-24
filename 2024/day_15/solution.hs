c64a9c4ab6a43ad844830ac8a9a67b246ba2c959
Lofty <prantik.ts@gmail.com>
2024-12-24 20:19:59 +0000

    feat: day 15

--- 2024/day_15/solution.hs
+++ 2024/day_15/solution.hs
@@ -1,8 +1,15 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use second" #-}
+
 module Main where
 
+-- debug and improve p2
+
 import qualified Data.List as L
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
+import Debug.Trace (traceShowId)
 import System.Environment (getArgs)
 
 type Coords = (Int, Int) -- ( y, x )
@@ -10,6 +17,18 @@ data Board = Board
   { look :: M.Map Coords Char
   , bot :: Coords
   }
+
+instance Show Board where
+  show b = L.intercalate "\n" grid ++ "\n\nBot @ " ++ show (bot b)
+    where
+      grid = chunks (fst ends + 1) . map snd $ M.toAscList merged'
+      merged' = M.insert (bot b) '@' merged
+      merged = foldr (uncurry M.insert) empti elements
+      empti = M.fromList $ [((y, x), '.') | x <- [0 .. fst ends], y <- [0 .. snd ends]]
+      elements = M.toDescList $ look b
+      ends = (20, 20)
+      chunks _ [] = []
+      chunks n x = (\(ys, zs) -> ys : chunks n zs) $ splitAt n x
 
 type Input = (Board, String)
 
@@ -23,14 +42,15 @@ parser x = (board, concat splitMoves)
       [((y, x), c) | (y, l) <- zip [0 ..] rb, (x, c) <- zip [0 ..] l]
     board =
       Board
-        { look = M.fromList $ filter (\(_, c) -> c `elem` "#O[]") markedBoard
+        { look = M.fromList $ filter (\(_, c) -> c `elem` "#O") markedBoard
         , bot = fst . fromJust $ L.find ((== '@') . snd) markedBoard
         }
 
 sumUp :: Board -> Int
 sumUp = M.foldrWithKey' stack 0 . look
   where
-    stack (x, y) 'O' t = t + 100 * x + y
+    stack (y, x) 'O' t = t + 100 * y + x
+    stack (y, x) '[' t = t + 100 * y + x
     stack _ _ t = t
 
 moveBot :: Board -> Char -> Board
@@ -39,10 +59,10 @@ moveBot b ch = case move b ch $ shift ch $ bot b of
   Nothing -> b
 
 shift :: Char -> Coords -> Coords
-shift '^' (x, y) = (y - 1, x)
-shift 'v' (x, y) = (y + 1, x)
-shift '<' (x, y) = (y, x - 1)
-shift '>' (x, y) = (y, x + 1)
+shift '^' (y, x) = (y - 1, x)
+shift 'v' (y, x) = (y + 1, x)
+shift '<' (y, x) = (y, x - 1)
+shift '>' (y, x) = (y, x + 1)
 
 move :: Board -> Char -> Coords -> Maybe Board
 move b ch l = case M.lookup l $ look b of
@@ -52,12 +72,43 @@ move b ch l = case M.lookup l $ look b of
     let l' = shift ch l
     b' <- move b ch l'
     return b'{look = M.insert l' 'O' $ M.delete l $ look b'}
+  Just '[' -> if ch `elem` "^v" then vertical '>' else horizontal '>'
+  Just ']' -> if ch `elem` "^v" then vertical '<' else horizontal '<'
+  where
+    vertical ch' = do
+      let l' = shift ch l
+          l'' = shift ch' l'
+          l''' = shift ch' l
+          br = braces ch'
+      b' <- move b ch l''
+      b'' <- move b' ch l'
+      let insertB = M.insert l' br . M.insert l'' (opp br)
+      return b''{look = insertB $ M.delete l $ M.delete l''' $ look b''}
+    horizontal ch' = do
+      let l' = shift ch' l
+          l'' = shift ch' l'
+          br = braces ch'
+      b' <- move b ch' l''
+      let insertB = M.insert l' br . M.insert l'' (opp br)
+      return b'{look = insertB $ M.delete l $ look b'}
+    braces br = if br == '>' then '[' else ']'
+    opp '[' = ']'
+    opp ']' = '['
+
+expand :: Input -> Input
+expand (b, s) = (Board{look = look', bot = bot'}, s)
+  where
+    look' = foldr addPoint M.empty . M.toList $ look b
+    addPoint (co, '#') = insert2x co '#' '#'
+    addPoint (co, 'O') = insert2x co '[' ']'
+    insert2x (y, x) a b = M.insert (y, 2 * x) a . M.insert (y, 2 * x + 1) b
+    bot' = (\(y, x) -> (y, 2 * x)) $ bot b
 
 solve1 :: Input -> Solution
 solve1 = sumUp . uncurry (L.foldl' moveBot)
 
 solve2 :: Input -> Solution
-solve2 = error "Part 2 Not implemented"
+solve2 = sumUp . uncurry (L.foldl' moveBot) . expand
 
 main :: IO ()
 main = do
